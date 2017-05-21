912f254fa63751b46b040c8b9fbd4e9ecbaf71c0
David Yu <david.yu@docker.com>
2017-05-21 16:33:50 -0700

    adding more deploy steps

--- Vagrantfile
+++ Vagrantfile
@@ -165,8 +165,11 @@ Vagrant.configure(2) do |config|
         sudo apt-get install -y apt-transport-https ca-certificates ntpdate
         sudo ntpdate -s time.nist.gov
         sudo cp /vagrant/scripts/install_ee.sh .
+        sudo cp /vagrant/scripts/join_worker.sh .
         sudo chmod +x install_ee.sh
+        sudo chmod +x join_worker.sh
         ./install_ee.sh
+        ./join_worker.sh
      SHELL
     end
 
--- scripts/install_ucp.sh
+++ scripts/install_ucp.sh
@@ -3,6 +3,6 @@ export UCP_PASSWORD=$(cat /vagrant/ucp_password)
 docker run --rm --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:2.1.4 install --host-address ${UCP_IPADDR} --admin-password ${UCP_PASSWORD} --san ucp.local --license $(cat /vagrant/docker_subscription.lic)
 docker swarm join-token manager | awk -F " " '/token/ {print $2}' > /vagrant/swarm-join-token-mgr
 docker swarm join-token worker | awk -F " " '/token/ {print $2}' > /vagrant/swarm-join-token-worker
-docker run --rm --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:2.1.4 id | awk '{ print $1}' > /vagrant/ucp-vancouver-id
+docker run --rm --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:2.1.4 id | awk '{ print $1}' > /vagrant/ucp-id
 export UCP_ID=$(cat /vagrant/ucp-vancouver-id)
 docker run --rm -i --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:2.1.4 backup --id ${UCP_ID} --root-ca-only --passphrase "secret" > /vagrant/backup.tar
