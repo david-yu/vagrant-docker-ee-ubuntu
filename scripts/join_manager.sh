8ecdcc1a8ad646181f412096ce20cb808ca44ff4
David Yu <david.yu@docker.com>
2017-05-24 01:24:28 -0700

    refactor to join manager script

---
+++ scripts/join_manager.sh
@@ -0,0 +1,3 @@
+export UCP_IPADDR=$(cat /vagrant/ucp-node1-ipaddr)
+export SWARM_JOIN_TOKEN_MASTER=$(cat /vagrant/swarm-join-token-mgr)
+docker swarm join --token ${SWARM_JOIN_TOKEN_WORKER} ${UCP_IPADDR}:2377
