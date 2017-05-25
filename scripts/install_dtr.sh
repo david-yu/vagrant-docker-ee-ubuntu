d5d2f8469d7875d2ac2118c570f7c910372c2684
David Yu <david.yu@docker.com>
2017-05-25 16:16:29 -0700

    adding correct cert command

--- scripts/install_dtr.sh
+++ scripts/install_dtr.sh
@@ -4,10 +4,6 @@ export UCP_IPADDR=$(cat /vagrant/ucp-node1-ipaddr)
 export UCP_PASSWORD=$(cat /vagrant/ucp_password)
 export DTR_REPLICA_ID=$(cat /vagrant/dtr-replica-id)
 
-# Join Swarm
-export SWARM_JOIN_TOKEN_WORKER=$(cat /vagrant/swarm-join-token-worker)
-docker swarm join --token ${SWARM_JOIN_TOKEN_WORKER} ${UCP_IPADDR}:2377
-
 # Install DTR
 sudo curl -k https://${UCP_IPADDR}/ca > ucp-ca.pem
 docker run --rm docker/dtr:2.2.4 install --ucp-url https://${UCP_IPADDR} --ucp-node dtr-node1 --replica-id ${DTR_REPLICA_ID} --dtr-external-url https://dtr.local --ucp-username admin --ucp-password ${UCP_PASSWORD} --ucp-ca "$(cat ucp-ca.pem)"
@@ -15,6 +11,6 @@ docker run --rm docker/dtr:2.2.4 install --ucp-url https://${UCP_IPADDR} --ucp-n
 docker run --rm docker/dtr:2.2.4 backup --ucp-url https://${UCP_IPADDR} --existing-replica-id ${DTR_REPLICA_ID} --ucp-username admin --ucp-password ${UCP_PASSWORD} --ucp-ca "$(cat ucp-ca.pem)" > /tmp/backup.tar
 
 # Trust self-signed DTR CA
-sudo curl -k https://dtr.local/ca -o /etc/pki/ca-trust/source/anchors/dtr.local.crt
+sudo curl -k https://dtr.local/ca -o /usr/local/share/ca-certificates/dtr.local.crt
 sudo update-ca-certificates
 sudo service docker restart
