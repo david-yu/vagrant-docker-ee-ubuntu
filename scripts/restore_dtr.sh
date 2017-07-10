c3869d402a84024d4d9e9b414a0a1d6c54983565
David Yu <david.yu@docker.com>
2017-07-09 23:17:53 -0400

    adding fixes to restore script

---
+++ scripts/restore_dtr.sh
@@ -0,0 +1,9 @@
+export UCP_IPADDR=$(cat /vagrant/ucp-node1-ipaddr)
+export DTR_IPADDR=$(cat /vagrant/dtr-node1-ipaddr)
+export UCP_PASSWORD=$(cat /vagrant/ucp_password)
+
+curl -k https://${UCP_IPADDR}/ca > ucp-ca.pem
+docker run --rm docker/dtr:2.2.5 destroy --ucp-insecure-tls
+docker run --rm docker/dtr:2.2.5 restore --ucp-url ${UCP-IPADDR} --ucp-username admin \
+  --ucp-password dockeradmin --dtr-external-url https://dtr.local --ucp-node dtr-node1 \
+  --ucp-ca "$(cat ucp-ca.pem)"  < /tmp/backup.tar
