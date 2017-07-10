70f1b375cc54cc77bce7c70c07c5d2886b67ebe6
David Yu <david.yu@docker.com>
2017-07-10 02:08:30 -0400

    dont provision on restore

--- scripts/snapshot_restore_initial.sh
+++ scripts/snapshot_restore_initial.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-vagrant snapshot restore ucp-node1 ucp-node1-initial
-vagrant snapshot restore dtr-node1 dtr-node1-initial
-vagrant snapshot restore worker-node1 worker-node1-initial
-vagrant snapshot restore worker-node1 worker-node2-initial
+vagrant snapshot restore --no-provision ucp-node1 ucp-node1-initial
+vagrant snapshot restore --no-provision dtr-node1 dtr-node1-initial
+vagrant snapshot restore --no-provision worker-node1 worker-node1-initial
+vagrant snapshot restore --no-provision worker-node1 worker-node2-initial
