ff56f7926a4cc355dcd99319a8d8b8e7c88f2595
David Yu <david.yu@docker.com>
2017-08-21 13:55:03 -0700

    adding new version for docker ee

--- scripts/install_ucp.sh
+++ scripts/install_ucp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-export UCP_VERSION=2.1.4
+export UCP_VERSION=2.2.0
 
 ifconfig enp0s8 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}' > /vagrant/ucp-node1-ipaddr
 export UCP_IPADDR=$(cat /vagrant/ucp-node1-ipaddr)
 export UCP_PASSWORD=$(cat /vagrant/ucp_password)
 export UCP_USERNAME=$(cat /vagrant/ucp_username)
-docker run --rm --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:${UCP_VERSION} install --host-address ${UCP_IPADDR} --admin-username ${UCP_USERNAME}--admin-password ${UCP_PASSWORD} --san ucp.local --license $(cat /vagrant/docker_subscription.lic)
+docker run --rm --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:${UCP_VERSION} install --host-address ${UCP_IPADDR} --admin-username ${UCP_USERNAME} --admin-password ${UCP_PASSWORD} --san ucp.local --license $(cat /vagrant/docker_subscription.lic)
 docker swarm join-token manager | awk -F " " '/token/ {print $2}' > /vagrant/swarm-join-token-mgr
 docker swarm join-token worker | awk -F " " '/token/ {print $2}' > /vagrant/swarm-join-token-worker
 docker run --rm --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:${UCP_VERSION} id | awk '{ print $1}' > /vagrant/ucp-id
