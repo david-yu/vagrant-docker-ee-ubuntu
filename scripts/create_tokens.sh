58485b925204916c7755502dadb6ab383dbd7376
David Yu <david.yu@docker.com>
2017-08-21 15:13:07 -0700

    change join token script

--- scripts/create_tokens.sh
+++ scripts/create_tokens.sh
@@ -1,2 +1,2 @@
-docker swarm join-token manager | awk -F " " '/token/ {print $2}' > /vagrant/swarm-join-token-mgr
-docker swarm join-token worker | awk -F " " '/token/ {print $2}' > /vagrant/swarm-join-token-worker
+docker swarm join-token -q manager > /vagrant/swarm-join-token-mgr
+docker swarm join-token -q worker > /vagrant/swarm-join-token-worker
