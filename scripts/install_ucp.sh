aa0f01131376aae901055098b20d559eb80bbba3
David Yu <david.yu@docker.com>
2017-08-31 15:27:23 -0700

    adding patches for 17.06

--- scripts/install_ucp.sh
+++ scripts/install_ucp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export UCP_VERSION=2.2.0
+export UCP_VERSION=2.2.2
 
 ifconfig enp0s8 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}' > /vagrant/ucp-node1-ipaddr
 export UCP_IPADDR=$(cat /vagrant/ucp-node1-ipaddr)
--- scripts/upgrade_dtr.sh
+++ scripts/upgrade_dtr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DTR_VERSION=2.2.5
+export DTR_VERSION=2.3.2
 export UCP_IPADDR=$(cat /vagrant/ucp-vancouver-node1-ipaddr)
 export UCP_PASSWORD=$(cat /vagrant/ucp_password)
 export DTR_REPLICA_ID=$(cat /vagrant/dtr-replica-id)
@@ -9,5 +9,5 @@ docker pull docker/dtr:${DTR_VERSION}
 
 docker run -it --rm \
   docker/dtr:${DTR_VERSION} upgrade --ucp-url https://${UCP_IPADDR} \
-  --ucp-username admin --ucp-password ${UCP_PASSWORD} --existing-replica-id ${DTR_REPLICA_ID} \
+  --ucp-username admin --ucp-password ${UCP_PASSWORD} \
   --ucp-insecure-tls
