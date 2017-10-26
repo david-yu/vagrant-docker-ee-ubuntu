0795ec86e96c888bb010683c2a96c93d56c1ded7
Joe Gardiner <joe@Joes-MacBook-Pro.local>
2017-10-26 15:28:02 -0700

    Adds cert required for backup

--- scripts/backup_dtr.sh
+++ scripts/backup_dtr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export UCP_USERNAME=$(cat /vagrant/ucp_username)
 export UCP_PASSWORD=$(cat /vagrant/ucp_password)
 export UCP_IPADDR=$(cat /vagrant/ucp-node1-ipaddr)
 export DTR_IPADDR=$(cat /vagrant/dtr-node1-ipaddr)
@@ -7,4 +8,6 @@ export SWARM_JOIN_TOKEN_WORKER=$(cat /vagrant/swarm-join-token-worker)
 export DTR_REPLICA_ID=$(cat /vagrant/dtr-replica-id)
 export DTR_VERSION=2.3.3
 
-docker run --rm docker/dtr:${DTR_VERSION} backup --ucp-url https://${UCP_IPADDR} --existing-replica-id ${DTR_REPLICA_ID} --ucp-username admin --ucp-password ${UCP_PASSWORD} --ucp-ca "$(cat ucp-ca.pem)" > /tmp/backup.tar
+curl -k https://${UCP_IPADDR}/ca > ucp-ca.pem
+
+docker run --rm docker/dtr:${DTR_VERSION} backup --ucp-url https://${UCP_IPADDR} --existing-replica-id ${DTR_REPLICA_ID} --ucp-username ${UCP_USERNAME} --ucp-password ${UCP_PASSWORD} --ucp-ca "$(cat ucp-ca.pem)" > /tmp/backup.tar
