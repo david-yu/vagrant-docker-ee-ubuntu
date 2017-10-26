cb6e14cc42efc296c8f18baf247f7c234f8056e6
Joe Gardiner <joe@Joes-MacBook-Pro.local>
2017-10-26 15:29:19 -0700

    Fixed worker / manager env var

--- scripts/join_manager.sh
+++ scripts/join_manager.sh
@@ -2,4 +2,4 @@
 
 export UCP_IPADDR=$(cat /vagrant/ucp-node1-ipaddr)
 export SWARM_JOIN_TOKEN_MASTER=$(cat /vagrant/swarm-join-token-mgr)
-docker swarm join --token ${SWARM_JOIN_TOKEN_WORKER} ${UCP_IPADDR}:2377
+docker swarm join --token ${SWARM_JOIN_TOKEN_MASTER} ${UCP_IPADDR}:2377
