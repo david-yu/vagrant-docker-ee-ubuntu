927cd60c7b89f51853ccb2e8874197d6ebbef211
David Yu <david.yu@docker.com>
2018-01-08 15:52:54 -0800

    adding changes for importing project from github

--- scripts/configure_gitlab.sh
+++ scripts/configure_gitlab.sh
@@ -14,15 +14,7 @@ export SSH_KEY_GITLAB=$(cat files/id_rsa.pub)
 curl --insecure --request POST --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --header "Content-Type:application/json" https://gitlab.local/api/v3/users/2/keys -d "{ \"title\": \"docker-ssh-key\", \"key\": \"$SSH_KEY_GITLAB\" }"
 
 # Create Docker Node App project
-curl --insecure --request POST --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --header "Content-Type:application/json" https://gitlab.local/api/v3/projects/user/2 -d "{ \"name\": \"docker-node-app\" }"
-
-# Clone repo from Github and Push to gitlab.local
-git clone https://github.com/yongshin/docker-node-app
-cd docker-node-app
-git remote rename origin old-origin
-git remote add origin git@gitlab.local:root/docker-node-app.git
-GIT_SSH_COMMAND="ssh -i files/id_rsa" git push -u origin --all
-GIT_SSH_COMMAND="ssh -i files/id_rsa" git push -u origin --tags
+curl --insecure --request POST --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --header "Content-Type:application/json" https://gitlab.local/api/v3/projects/user/2 -d "{ \"name\": \"docker-node-app\", \"import_url\": \"https://github.com/yongshin/docker-node-app\" }"
 
 # Create Project webhook
 curl --insecure --request POST --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --header "Content-Type:application/json" https://gitlab.local/api/v3/projects/1/hooks -d "{ \"url\": \"http://jenkins.local\" }"
