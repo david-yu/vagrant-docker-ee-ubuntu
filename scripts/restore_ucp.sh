c9a5aaae4045cd621c582a5607f7ddd64b3fe4c9
David Yu <david.yu@docker.com>
2018-01-19 16:53:21 -0800

    restore fix

--- scripts/restore_ucp.sh
+++ scripts/restore_ucp.sh
@@ -7,7 +7,7 @@ export UCP_VERSION=3.0.0-beta2
 
 docker container run --rm -it \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  --name ucp docker/ucp:${UCP_VERSION} uninstall-ucp --id ${UCP_ID} --ucp-insecure-tls
+  --name ucp docker/ucp:${UCP_VERSION} uninstall-ucp --id ${UCP_ID} 
 
 docker container run --rm -i --name ucp \
   -v /var/run/docker.sock:/var/run/docker.sock  \
