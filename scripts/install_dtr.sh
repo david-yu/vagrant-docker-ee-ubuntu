d8c9ab14bdc44a60a6d3fc39a6ca6689c73a9d0f
David Yu <david.yu@docker.com>
2018-04-18 19:43:45 -0700

    changing username for dtr backup

--- scripts/install_dtr.sh
+++ scripts/install_dtr.sh
@@ -8,13 +8,13 @@ export UCP_PASSWORD=$(cat /vagrant/env/ucp_password)
 export DTR_REPLICA_ID=$(cat /vagrant/env/dtr-replica-id)
 export DTR_VERSION=2.5.0
 
-# Sleep 80 seconds to wait for node registration and for swarm node to come up healthy
-sleep 80
+# Sleep 90 seconds to wait for node registration and for swarm node to come up healthy
+sleep 90
 
 # Install DTR
 sudo -E sh -c 'docker run --rm docker/dtr:${DTR_VERSION} install --ucp-url https://"${UCP_IPADDR}" --ucp-node dtr --replica-id "${DTR_REPLICA_ID}" --dtr-external-url https://dtr.local --ucp-username "${UCP_USERNAME}" --ucp-password "${UCP_PASSWORD}" --ucp-insecure-tls'
 # Run backup of DTR
-sudo -E sh -c 'docker run --rm --log-driver none docker/dtr:${DTR_VERSION} backup --ucp-url https://"${UCP_IPADDR}" --existing-replica-id "${DTR_REPLICA_ID}" --ucp-username admin --ucp-password "${UCP_PASSWORD}" --ucp-insecure-tls > /tmp/backup.tar'
+sudo -E sh -c 'docker run --rm --log-driver none docker/dtr:${DTR_VERSION} backup --ucp-url https://"${UCP_IPADDR}" --existing-replica-id "${DTR_REPLICA_ID}" --ucp-username "${UCP_USERNAME}" --ucp-password "${UCP_PASSWORD}" --ucp-insecure-tls > /tmp/backup.tar'
 
 # Sleep 30 seconds to wait for DTR to come back up
 sleep 30
