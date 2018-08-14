d43e0f78fb50367b771943a0a54eca94dd7c7500
David Yu <david.yu@docker.com>
2018-08-14 15:04:36 -0700

    changing manager join to work

--- scripts/join_manager.sh
+++ scripts/join_manager.sh
@@ -2,4 +2,4 @@
 
 export UCP_IPADDR=$(cat /vagrant/env/ucp-node1-ipaddr)
 export SWARM_JOIN_TOKEN_MASTER=$(cat /vagrant/env/swarm-join-token-mgr)
-docker swarm join --token ${SWARM_JOIN_TOKEN_WORKER} ${UCP_IPADDR}:2377
+docker swarm join --token ${SWARM_JOIN_TOKEN_MASTER} ${UCP_IPADDR}:2377
